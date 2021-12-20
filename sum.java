d0451bdd00c0533e88d9daac7de6313113f5f493
SAKTHIVISHNU25 <sakthivishnu2002@gmail.com>
2021-12-21 00:36:41 +0530

    The java sum is

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class sum{
+    public static void main(String[] args){
+        int a,b,c;
+        System.out.println("Enter the value of a");
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        System.out.println("Enter the value of b");
+        b = sc.nextInt();
+        c=a+b;
+        System.out.println("the sum is: "+c);
+    }
+}
