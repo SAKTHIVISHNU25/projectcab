891822ae97fcf15cad3964640c86f04df9f7efcd
SAKTHIVISHNU25 <sakthivishnu2002@gmail.com>
2021-12-21 08:54:53 +0530

    subtraction is added:

--- sum.java
+++ sum.java
@@ -1,13 +1,14 @@
 import java.util.*;
 public class sum{
     public static void main(String[] args){
-        int a,b,c;
+        int a,b,c,d;
         System.out.println("Enter the value of a");
         Scanner sc = new Scanner(System.in);
         a = sc.nextInt();
         System.out.println("Enter the value of b");
         b = sc.nextInt();
         c=a+b;
+        d=a-b;
         System.out.println("the sum is: "+c);
     }
 }
